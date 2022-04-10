7acb2fa8b2f88a1d1e07d4ba55a9e85984fb8b75
Dev deRev <devderev@free.fr>
2022-04-10 19:30:57 +0200

    iteration1 done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { BrowserRouter as Router } from 'react-router-dom'
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import AppMain from './components/layout/AppMain';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import HomePage from './components/pages/HomePage';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Router>
-        <AppMain/>
+        <Routes>
+        <Route path='/' element={<HomePage/> }/>
+				{/* <Route path="/*" element={<h1>404</h1>} /> */}
+        </Routes>
       </Router>
     </div>
   );
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,16 +1,40 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import allBeers from '../../assets/beers.png'
+import randomBeers from '../../assets/random-beer.png'
+import newBeers from '../../assets/new-beer.png'
+import './HomePage.css'
+
 
 const HomePage = () => {
     console.log('COUCOU COUCOU')
   return (
     <div>
-        <p>COUCOU COUCOU</p>
-        <div class="card bg-dark text-white">
-            <img src='../../../src/assets/beers.png' class="card-img" alt="ALL BEERS"/>
-            <div class="card-img-overlay">
-                <h3 class="card-title">ALL BEERS</h3>
-                <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias libero culpa pariatur ipsam laborum fuga voluptates quis possimus laudantium labore, nemo illo doloremque nisi excepturi totam vel quas molestiae natus. </p>
+        <div className="card bg-dark text-white">
+           <Link to='/beers'>
+           <img src={allBeers} className="card-img" alt="ALL BEERS"/>
+           </Link> 
+            <div className="card-img-overlay">
+                <h3 className="card-title btn btn-primary">ALL BEERS</h3>
+                <p className="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias libero culpa pariatur ipsam laborum fuga voluptates quis possimus laudantium labore, nemo illo doloremque nisi excepturi totam vel quas molestiae natus. </p>
+            </div>
+        </div>
+        <div className="card bg-dark text-white">
+           <Link to='/beers'>
+           <img src={randomBeers} className="card-img" alt="RANDOM BEERS"/>
+           </Link> 
+            <div className="card-img-overlay">
+                <h3 className="card-title btn btn-primary">RANDOM BEERS</h3>
+                <p className="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias libero culpa pariatur ipsam laborum fuga voluptates quis possimus laudantium labore, nemo illo doloremque nisi excepturi totam vel quas molestiae natus. </p>
+            </div>
+        </div>
+        <div className="card bg-dark text-white">
+           <Link to='/beers'>
+           <img src={newBeers} className="card-img" alt="NEW BEERS"/>
+           </Link> 
+            <div className="card-img-overlay">
+                <h3 className="card-title btn btn-primary">NEW BEERS</h3>
+                <p className="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias libero culpa pariatur ipsam laborum fuga voluptates quis possimus laudantium labore, nemo illo doloremque nisi excepturi totam vel quas molestiae natus. </p>
             </div>
         </div>
     </div>
