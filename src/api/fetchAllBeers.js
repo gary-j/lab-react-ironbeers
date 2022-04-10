41d20feb1854e67d01f98450daf5358bbdd63a0e
Dev deRev <devderev@free.fr>
2022-04-10 21:56:45 +0200

    iteration 3 qui marche pas SMLP

---
+++ src/api/fetchAllBeers.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://ih-beers-api2.herokuapp.com/beers'
+
+const getBeersFromApi = async ()=>{
+    // Api renvoi un tableau d'objet [{}]
+    const {data} = await axios.get(BASE_URL)
+    return data
+}
+
+export {getBeersFromApi};
--- src/components/pages/Beers.js
+++ src/components/pages/Beers.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { getBeersFromApi } from "../../api/fetchAllBeers";
+import "./Beers.css";
 
 const Beers = () => {
+  const [allBeers, setAllBeers] = useState([]);
+  useEffect(() => {
+    getBeersFromApi.then(data => setAllBeers(data));
+  }, []);
+  console.log(allBeers, 'ALL BEEEEARS')
+  
   return (
-    <div>Beers</div>
-  )
-}
+      
+    <main>
+      {allBeers.map((beer) => {
+        <div key={beer._id} className="card mb-3">
+          <div classNames="row g-0">
+            <div classNames="col-md-4">
+              <img src={beer.image_url} classNames="img-fluid rounded-start" alt={beer.name} />
+            </div>
+            <div classNames="col-md-8">
+              <div classNames="card-body">
+                <h5 classNames="card-title">{beer.name}</h5>
+                <p classNames="card-text">
+                  {beer.tagline}
+                </p>
+                <p classNames="card-text">
+                  <small classNames="text-muted">{beer.contributed_by}</small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>;
+      })}
+    </main>
+  );
+};
 
-export default Beers
+export default Beers;
